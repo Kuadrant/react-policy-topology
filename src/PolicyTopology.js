349884368203981fd925224b8a66f4845ad86e8f
Jason Madigan <jason@jasonmadigan.com>
2024-08-07 09:59:57 +0100

    <PolicyTopology> component
    Signed-off-by: Jason Madigan <jason@jasonmadigan.com>

---
+++ src/PolicyTopology.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from 'react';
+import { instance } from '@viz-js/viz';
+
+const PolicyTopology = ({ dot }) => {
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        if (containerRef.current && dot) {
+            instance().then(viz => {
+                return viz.renderSVGElement(dot);
+            }).then(svgElement => {
+                containerRef.current.innerHTML = '';
+                containerRef.current.appendChild(svgElement);
+            }).catch(error => {
+                console.error('Error rendering DOT file:', error);
+            });
+        }
+    }, [dot]);
+
+    return (
+        <div ref={containerRef} className="policy-topology-container" />
+    );
+};
+
+export default PolicyTopology;
